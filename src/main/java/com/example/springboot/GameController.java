757514eca39ce75ec1d31cffc5dcbd5fd73de408
karanjain113 <karanj@tekion.com>
2020-01-22 09:57:52 +0530

    Updated

--- src/main/java/com/example/springboot/GameController.java
+++ src/main/java/com/example/springboot/GameController.java
@@ -7,12 +7,26 @@ public class GameController {
 
     @RequestMapping("/")
     public String index() {
-        return "<button onclick=\"window.location.href='result'\">Start Match</button>";
+        return "<h2>Cricket Match</h2>\n" +
+                "\n" +
+                "<form action=\"/result\">\n" +
+                "  Team1 name:<br>\n" +
+                "  <input type=\"text\" name=\"Team1\" value=\"\">\n" +
+                "  <br>\n" +
+                "  Team2 name:<br>\n" +
+                "  <input type=\"text\" name=\"Team2\" value=\"\">\n" +
+                "  <br>\n" +
+                "  Overs:<br>\n" +
+                "  <input type=\"text\" name=\"Overs\" value=\"\">\n" +
+                "  <br><br>\n" +
+                "  <input type=\"submit\" value=\"Submit\">\n" +
+                "</form> ";
     }
     @RequestMapping("/result")
-    public Match post()
+    @ResponseBody
+    public Match post(@RequestParam("Team1") String TeamA,@RequestParam("Team2") String TeamB,@RequestParam("Overs") String Overs)
     {
-        Match m=new Match(new Team("India"),new Team("Australia"),50);
+        Match m=new Match(new Team(TeamA),new Team(TeamB),Integer.parseInt(Overs));
         m.startMatch();
         return m;
     }
